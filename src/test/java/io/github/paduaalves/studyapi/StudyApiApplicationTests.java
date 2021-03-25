5694840626f39027206416875d24a336103a0d67
Pádua Alves <paduaalves03@gmail.com>
2021-03-25 14:41:27 -0300

    Criando projeto para estudo de spring

---
+++ src/test/java/io/github/paduaalves/studyapi/StudyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.paduaalves.studyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
